e8016e1cc0d07c80fc49fecd5652c23ee3d1b20d
kethakishore143 <kishore1993keta@gmail.com>
2024-06-28 00:09:59 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TimeframeChart from './components/TimeframeChart';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="chartheading">Timeframe Chart</h1>
+      <TimeframeChart />
+    </div>
+  );
+};
+
+export default App;
